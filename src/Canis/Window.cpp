ff6318d8892a222d83f812ca56d831967cb7e7a7
EricWRogers <ericwilliamrogers@gmail.com>
2025-02-07 10:48:22 -0600

    WINDOWgit add -Agit add -A?

--- src/Canis/Window.cpp
+++ src/Canis/Window.cpp
@@ -1,5 +1,7 @@
 #include "Window.hpp"
 #include <SDL.h>
+#include <GL/glew.h>
+#include <iostream>
 
 namespace Canis
 {
@@ -18,13 +20,49 @@ namespace Canis
             SDL_WINDOWPOS_CENTERED,
             SDL_WINDOWPOS_CENTERED, 
             m_width, m_height, flags);
+        
+        if (m_sdlWindow == nullptr)
+        {
+            return 1; // failed to open window
+        }
+
+        m_glContext = (void*)SDL_GL_CreateContext((SDL_Window*)m_sdlWindow);
+
+        if (m_glContext == nullptr)
+        {
+            return 2; // failed to create gl context
+        }
+
+        GLenum error = glewInit();
+
+        if (error != GLEW_OK)
+        {
+            return 3;
+        }
+
+        const char* openGLVersion = (const char*)glGetString(GL_VERSION);
+        std::cout << openGLVersion << std::endl;
+
+        ClearColor();
+
+        // VSYNC 0 off 1 on
+        SDL_GL_SetSwapInterval(1);
+
+        // Enable alpha blending
+        glEnable(GL_BLEND);
+        glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
 
         return 0;
     }
+
     void Window::Swap() {
-
+        
     }
-    void Window::ClearColor() {
 
+    void Window::ClearColor() {
+        // before a new frame is drawn we need to clear the buffer
+        // the clear color will be the new value of all of the pixels
+        // in that buffer
+        glClearColor(0.05f, 0.05f, 0.05f, 1.0f);
     }
 }
--- src/main.cpp
+++ src/main.cpp
@@ -25,7 +25,7 @@ int main(int argc, char* argv[])
     Canis::Window window;
     window.Create();
     
-    bool running = false;
+    bool running = true;
 
     while(running)
     {
