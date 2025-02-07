8443dcc96ce0478f08e66a485088b4bfb5232cb6
EricWRogers <ericwilliamrogers@gmail.com>
2025-02-07 10:21:58 -0600

    attrib

--- src/Canis/Window.cpp
+++ src/Canis/Window.cpp
@@ -7,11 +7,18 @@ namespace Canis
         m_width = 640;
         m_height = 360;
 
-        SDL_CreateWindow("Computer Graphics",
+        Uint32 flags = SDL_WINDOW_OPENGL;
+
+        SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
+        SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 3);
+        SDL_GL_SetAttribute(SDL_GL_STENCIL_SIZE, 8);
+        SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
+
+        m_sdlWindow = (void*)SDL_CreateWindow("Computer Graphics",
             SDL_WINDOWPOS_CENTERED,
             SDL_WINDOWPOS_CENTERED, 
-            m_width, m_height, 0);
-            
+            m_width, m_height, flags);
+
         return 0;
     }
     void Window::Swap() {
