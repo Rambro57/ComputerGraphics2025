05980972e9dbf953cea9516347fa0451c11213ff
EricWRogers <ericwilliamrogers@gmail.com>
2025-02-17 09:44:21 -0600

    ready your triangles

--- src/main.cpp
+++ src/main.cpp
@@ -17,11 +17,32 @@
 // git fetch
 // git pull
 
+// move out to external class
+unsigned int vertexShader;
+unsigned int shaderProgram;
+unsigned int VBO, VAO;
+
+const char *vertexShaderSource = "#version 330 core\n"
+                                 "layout (location = 0) in vec3 aPos;\n"
+                                 "void main()\n"
+                                 "{\n"
+                                 "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+                                 "}\0";
+
+const char *fragmentShaderSource = "#version 330 core\n"
+                                   "out vec4 FragColor;\n"
+                                   "void main()\n"
+                                   "{\n"
+                                   "   FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+                                   "}\0";
+
+void InitShader();
+
 #ifdef _WIN32
 #define main SDL_main
-extern "C" int main(int argc, char* argv[])
+extern "C" int main(int argc, char *argv[])
 #else
-int main(int argc, char* argv[])
+int main(int argc, char *argv[])
 #endif
 {
     Canis::Init();
@@ -31,20 +52,69 @@ int main(int argc, char* argv[])
 
     Canis::InputManager inputManager;
 
-    while(inputManager.Update(window.GetScreenWidth(), window.GetScreenHeight()))
+    while (inputManager.Update(window.GetScreenWidth(), window.GetScreenHeight()))
     {
-        
 
         glClearColor(
-            inputManager.mouse.x/window.GetScreenWidth(),
-            inputManager.mouse.y/window.GetScreenHeight(),
-            sin((float)SDL_GetTicks()/1000.0f), 1.0f);
-        
+            inputManager.mouse.x / window.GetScreenWidth(),
+            inputManager.mouse.y / window.GetScreenHeight(),
+            sin((float)SDL_GetTicks() / 1000.0f), 1.0f);
+
         glClear(GL_COLOR_BUFFER_BIT);
 
-        //Canis::Log("Mouse Pos: " + glm::to_string(mousePos));
+        // draw first triangle
+        
+
+        // Canis::Log("Mouse Pos: " + glm::to_string(mousePos));
         window.SwapBuffer();
     }
 
     return 0;
+}
+
+void InitShader()
+{
+    // build and compile our shader program
+    // ------------------------------------
+    // vertex shader
+    vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+    glCompileShader(vertexShader);
+    // check for shader compile errors
+    int success;
+    char infoLog[512];
+    glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+    if (!success)
+    {
+        glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+        std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n"
+                  << infoLog << std::endl;
+    }
+    // fragment shader
+    unsigned int fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+    glCompileShader(fragmentShader);
+    // check for shader compile errors
+    glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
+    if (!success)
+    {
+        glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
+        std::cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n"
+                  << infoLog << std::endl;
+    }
+    // link shaders
+    shaderProgram = glCreateProgram();
+    glAttachShader(shaderProgram, vertexShader);
+    glAttachShader(shaderProgram, fragmentShader);
+    glLinkProgram(shaderProgram);
+    // check for linking errors
+    glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+    if (!success)
+    {
+        glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
+        std::cout << "ERROR::SHADER::PROGRAM::LINKING_FAILED\n"
+                  << infoLog << std::endl;
+    }
+    glDeleteShader(vertexShader);
+    glDeleteShader(fragmentShader);
 }
